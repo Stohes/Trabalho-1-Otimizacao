6b74a42c20e2643879e7e6b58145fc057280b789
Stohes <T.Chies@edu.pucrs.br>
2022-08-23 23:19:43 -0300

    Versao inicial, iguala os tamanhos, divide em 3 partes iguais e soma, carry nao funciona e tem muitos ::-1

---
+++ Trabalho1.py
@@ -0,0 +1,37 @@
+import textwrap
+# import sys
+# num = str(sys.argv[1])
+# num2 = str(sys.argv[2])
+
+
+num = "18"
+num2 = "23"
+
+
+while (len(num) % 3) != 0:
+    num = "0" + num
+
+while (len(num2) % 3) != 0:
+    num2 = "0" + num2
+
+while len(num) != len(num2):
+    if len(num) > len(num2):
+        num2 = "0" + num2
+    else:
+        num = "0" + num
+
+# print(textwrap.wrap(num, int(len(num) / 3)))
+# print(textwrap.wrap(num2, int(len(num2) / 3)))
+
+
+def add(num, num2):
+    newNum = ""
+    for i in range(len(num)):
+        if (int(num[::-1][i]) + int(num2[::-1][i])) > 9:
+            newNum = str((int(num[::-1][i]) + int(num2[::-1][i]))) + newNum
+        else:
+            newNum = str((int(num[::-1][i]) + int(num2[::-1][i]))) + newNum
+    return newNum
+
+
+print(add(num, num2))
