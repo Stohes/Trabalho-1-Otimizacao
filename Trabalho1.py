d55517c041d6b45663944101645d080a2fc2d651
Stohes <T.Chies@edu.pucrs.br>
2022-08-24 15:46:15 -0300

    Divide todo cagado

--- Trabalho1.py
+++ Trabalho1.py
@@ -4,7 +4,7 @@ import textwrap
 # num2 = str(sys.argv[2])
 
 
-num = "18"
+num = "12345"
 num2 = "24"
 
 
@@ -28,7 +28,6 @@ def add(num, num2):
     newNum = ""
     carry = 0
     for i in range(len(num) - 1, -1, -1):
-
         if int(num[i]) + int(num2[i]) + carry > 9:
             if carry == 1:
                 newNum = str(int(num[i]) + int(num2[i]) + 1)[1] + newNum
@@ -44,4 +43,26 @@ def add(num, num2):
     return newNum
 
 
+def divide(num):
+    if (len(num[0]) == 1) and (isinstance(num, list)):
+        return num
+
+    if (isinstance(num, list)) and (len(num[0]) > 2):
+        for i in range(len(num)):
+            num[i] = textwrap.wrap(num, int(len(num) / 3))
+            return num[i]
+    else:
+        for i in num:
+            return list(i)
+
+    num = textwrap.wrap(num, int(len(num) / 3))
+
+    return divide(num)
+
+
 print(add(num, num2))
+print(divide(num))
+
+
+# for i in range(0, len(num), 2):
+#     return int(num[i]) * int(num[i + 1])
