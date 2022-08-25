7c566cb26905293cedf9b2afb77e85ccfd3f1e30
Stohes <T.Chies@edu.pucrs.br>
2022-08-24 23:34:44 -0300

    Codigo com ajuda do luigi, quase funcionando

--- KaratsubaString.py
+++ KaratsubaString.py
@@ -1,7 +1,7 @@
-num = "633333"
-num2 = "24"
-
-
+num = "1234567"
+num2 = "32145"
+# 9.685.156.215
+# 38.675.145.215
 while (len(num) % 3) != 0:
     num = "0" + num
 
@@ -15,11 +15,45 @@ while len(num) != len(num2):
         num = "0" + num
 
 
+def add(num, num2):
+    newNum = ""
+    carry = 0
+    num = str(num)
+    num2 = str(num2)
+
+    while len(num) != len(num2):
+        if len(num) > len(num2):
+            num2 = "0" + num2
+        else:
+            num = "0" + num
+
+    print("]] " + num)
+    print("]] " + num2)
+    for i in range(len(num) - 1, -1, -1):
+        if int(num[i]) + int(num2[i]) + carry > 9:
+            if carry == 1:
+                newNum = str(int(num[i]) + int(num2[i]) + 1)[1] + newNum
+            else:
+                newNum = str(int(num[i]) + int(num2[i]))[1] + newNum
+            carry = 1
+        else:
+            if carry == 1:
+                newNum = str(int(num[i]) + int(num2[i]) + 1) + newNum
+                carry = 0
+            else:
+                newNum = str(int(num[i]) + int(num2[i])) + newNum
+    print("--------------")
+    print(newNum)
+    return newNum
+
+
 def karatsuba(a, b):
     if len(str(a)) == 1:
-        return int(a) * int(b)
+        return str(int(a) * int(b))
 
     third = len(str(a)) // 3
+
+    # if third == 0: mudar pra 9 la em cima
 
     a1 = a[0:third]
     a2 = a[third:third*2]
@@ -28,19 +62,29 @@ def karatsuba(a, b):
     b2 = b[third:third*2]
     b3 = b[third*2:]
 
-    a1b1 = karatsuba(a1, b1)
-    a1b2 = karatsuba(a1, b2)
-    a1b3 = karatsuba(a1, b3)
-    a2b1 = karatsuba(a2, b1)
-    a2b2 = karatsuba(a2, b2)
-    a2b3 = karatsuba(a2, b3)
-    a3b1 = karatsuba(a3, b1)
-    a3b2 = karatsuba(a3, b2)
+    a1b1 = karatsuba(a1, b1) + ((len(a) - len(a1) + len(b) - len(b1))) * "0"
+    a1b2 = karatsuba(a1, b2) + (len(a) - len(a1) + len(b) - len(b1) - len(b2)) * "0"
+    a1b3 = karatsuba(a1, b3) + (len(a) - len(a1)) * "0"
+    a2b1 = karatsuba(a2, b1) + (len(a) - len(a1) - len(a2) + len(b) - len(b1)) * "0"
+    a2b2 = karatsuba(a2, b2) + (len(a) - len(a1) - len(a2) + len(b) - len(b1) - len(b2)) * "0"
+    a2b3 = karatsuba(a2, b3) + (len(a) - len(a1) - len(a2)) * "0"
+    a3b1 = karatsuba(a3, b1) + (len(b) - len(b1)) * "0"
+    a3b2 = karatsuba(a3, b2) + (len(b) - len(b1) - len(b2)) * "0"
     a3b3 = karatsuba(a3, b3)
 
-    return a1b1 + a1b2 + a1b3 + a2b1 + a2b2 + a2b3 + a3b1 + a3b2 + a3b3
+    print("--------")
+    print(a1b1)
+    print(a1b2)
+    print(a1b3)
+    print(a2b1)
+    print(a2b2)
+    print(a2b3)
+    print(a3b1)
+    print(a3b2)
+    print(a3b3)
+    return add(a1b1, add(a1b2, add(a1b3, add(a2b1, add(a2b2, add(a2b3, add(a3b1, add(a3b2, a3b3))))))))
 
 
 print(num)
 print(num2)
-print(karatsuba(int(num), int(num2)))
+print(karatsuba(num, num2))
